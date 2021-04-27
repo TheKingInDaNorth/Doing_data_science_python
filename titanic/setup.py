14f7eed4e6031a7c0524661eb15a6d1e1851fe91
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-04-27 14:51:49 -0700

    4,27,21

---
+++ titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='[A[Btitanic data science challenge',
+    author='titanic data science challenge',
+    license='MIT',
+)
